474bf898aa314932793a3c6f9501152a47f9f4b3
werner <werner_a_e@yahoo.es>
2019-06-11 19:03:06 -0300

    Add register route and basic template

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const rust = import('./pkg/front_raw_mystore');
 
-rust
-  .then(m => {
-      return m.run().then((data) => {
-          console.log(data);
-
-          console.log("The latest commit to the wasm-bindgen %s branch is:", data.name);
-          console.log("%s, authored by %s <%s>", data.commit.sha, data.commit.commit.author.name, data.commit.commit.author.email);
-      })
-  })
-.catch(console.error);
+rust.catch(console.error);
--- src/javascripts/router.js
+++ src/javascripts/router.js
@@ -1,6 +1,6 @@
-export function init_router() {
+export function init_router(app) {
     var router = new Navigo();
     router.on('register', function() {
-
+        app.innerHTML = '<object type="text/html" data="templates/register.html" ></object>';
     })
 }
