ff7a697cee9d27cb0b471ed461b95883a00e1156
werner <werner_a_e@yahoo.es>
2019-06-11 17:38:26 -0300

    Add basic dom manipulation (app)

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,6 @@
+use wasm_bindgen::prelude::*;
 use serde::{Deserialize, Serialize};
 mod fetch;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Branch {
-    pub name: String,
-    pub commit: Commit,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Commit {
-    pub sha: String,
-    pub commit: CommitDetails,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CommitDetails {
-    pub author: Signature,
-    pub committer: Signature,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Signature {
-    pub name: String,
-    pub email: String,
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RegisterUser {
@@ -31,4 +8,15 @@ pub struct RegisterUser {
     pub company: String,
     pub password: String,
     pub password_confirmation: String
+}
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let app = document.get_element_by_id("app");
+
+    Ok(())
 }
